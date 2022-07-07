b6f3db4c7c8d1431d2d905b21fe178164da97714
Heen <agny_@naver.com>
2022-07-07 18:05:23 +0900

    react_intro

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import logo from "./logo.svg";
+import styled from "styled-components";
+// import {Route, Routes} from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./Home";
+import Review from "./Review";
+
+function App() {
+  return (
+    <div className="App">
+      <Container>
+        <Router>
+          <Routes>
+            <Route path="/*" element={<Home />}></Route>
+            <Route path="/review/:day" element={<Review />}></Route>
+          </Routes>
+        </Router>
+      </Container>
+    </div>
+  );
+}
+
+const Container = styled.div`
+  width: 80vw;
+  max-width: 350px;
+  height: 90vh;
+  margin: 5vh auto;
+  border: 1px solid lightgray;
+  border-radius: 5px;
+  padding: 5vh 0;
+  box-sizing: border-box;
+  overflow: auto;
+  text-align: center;
+`;
+
+export default App;
